c64c54d5dce0bc847ea9221b4f9d334d03512c82
= <amilmammadov.m@gmail.com>
2023-05-24 12:56:21 +0400

    Entrance page login and signup part

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static TextStyle getTextStyle() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold);
+  }
+}
