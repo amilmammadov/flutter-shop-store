ff6a0ef99c6b38920e6785a822562859a1a3276b
= <amilmammadov.m@gmail.com>
2023-05-24 13:15:19 +0400

    add textfields in sign in page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop_store_1/pages/sign_up_page.dart';
+
+import '../constants.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -40,6 +43,203 @@ class _SignInPageState extends State<SignInPage> {
                 ],
               ),
             ),
+            Container(
+                padding: EdgeInsets.all(width),
+                child: Column(children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Email or Phone Number',
+                        style: Constants.getTextStyle(),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(15),
+                              hintText: 'amilmammadov.m@gmail.com',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Password',
+                        style: Constants.getTextStyle(),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              suffixIcon: const Icon(
+                                Icons.remove_red_eye_outlined,
+                                color: Colors.black,
+                              ),
+                              contentPadding: const EdgeInsets.all(15),
+                              hintText: '***********',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          'Forgot Password?',
+                          style: Constants.getTextStyle(),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    width: MediaQuery.of(context).size.width - 2 * width,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      'Sign in',
+                      style: Constants.getTextStyle(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  const Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                        color: Colors.black,
+                        thickness: 2,
+                        height: 4,
+                      )),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Text(
+                          'OR',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          child: Divider(
+                        color: Colors.black,
+                        thickness: 2,
+                        height: 4,
+                      )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      height: 45,
+                      width: MediaQuery.of(context).size.width - 2 * width,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 0.5),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 15,
+                            width: 15,
+                            child: Image.network(
+                              'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Text(
+                            textAlign: TextAlign.center,
+                            'Continue with Google',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      height: 45,
+                      width: MediaQuery.of(context).size.width - 2 * width,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 0.5),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.facebook,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            textAlign: TextAlign.center,
+                            'Continue with Facebook',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                              color: Colors.blue.shade900,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                      )
+                    ],
+                  )
+                ]))
           ]))
     ])));
   }
